26b867ce5c379c1f1ecb6ab099bd4144e90a58a5
Trương Mạnh Đạt <truongmanhdat061@gmail.com>
2026-09-14 21:05:49 +0700

    Time: 0 ms (100%), Space: 11.7 MB (94.18%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string res = "";
+        for(int j=0; j<strs[0].size(); j++){
+            char c = strs[0][j];
+
+            for(int i=1; i<strs.size(); i++){
+                if(strs[i][j] != c){
+                    return res;
+                }
+            }
+            res += c;
+        }
+        return res;
+    }
+};
